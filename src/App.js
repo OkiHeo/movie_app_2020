60fb3b3f94188d895ba78d8323b50be285441a1e
okayOki <okayoki2484@gmail.com>
2020-12-30 00:07:47 +0900

    ~04 props까지 학습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Food(props){
+  const { name, picture, ratings } = props;  // 구조 분해 할당. props에 포함된 데이터의 개수가 많아질수록 편하다.
+  return (
+    <div>
+      <img alt="" src={picture} />
+      <h2>I like {name}</h2>
+      <h4>{ratings}</h4>
+    </div>
+  );
+}
+
+
+Food.propTypes = {
+	name: PropTypes.string.isRequired,
+	picture: PropTypes.string.isRequired,
+	ratings: PropTypes.number,
+};
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'kimchi',
+    rating: 3,
+    image: 'http://image.auction.co.kr/itemimage/1b/45/1c/1b451c3fa6.jpg',
+  },
+  {
+    id: 2,
+    name: '순두부찌개',
+    rating: 4.8,
+    image: 'https://i.ytimg.com/vi/iCRj9VWDCt4/maxresdefault.jpg',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <h1>Hello</h1>
+      { foodILike.map( dish=>(
+        <Food id={dish.id} name={dish.name} picture={dish.image} ratings={dish.rating} />
+         )) }
+    </div>)
+  ;
 }
 
 export default App;
