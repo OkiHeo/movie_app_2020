9f0bb6cc40421f263b3b3483d2fe93f699052797
okayOki <okayoki2484@gmail.com>
2021-01-05 23:53:58 +0900

    Navigation 생성, css 적용.
    Link 컴포넌트의 사용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {HashRouter, Route} from 'react-router-dom';
+import Navigation from './components/Navigation'
 import About from './routes/About';
 import Home from './routes/Home';
 
@@ -8,6 +9,7 @@ class App extends React.Component {
   render(){
     return (
       <HashRouter>
+        <Navigation />
         <Route path="/about" component={About}/>
         <Route path="/"  exact={true} component={Home} />
       </HashRouter>
