eae4c9c2b6d5ba920e4e0ec84eaba53282217d49
okayOki <okayoki2484@gmail.com>
2021-01-05 23:40:16 +0900

    css 적용. Route 설정

--- src/App.js
+++ src/App.js
@@ -1,51 +1,16 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
-import Movie from './Movie';
+import {HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
 
 class App extends React.Component {
-  state = {
-    isLoading: true,    // 영화 데이터 로딩 상태(로딩 중: true)
-    movies: [],         // 이 곳에 로딩한 영화 정보를 저장.
-  };
-
-  getMovies = async () => {
-    const { data: { data: { movies }, }, } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    this.setState({ movies: movies, isLoading: false });
-  }
-
-  // 컴포넌트 마운트 6초 후에 로딩 상태 변경.
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-
+  render(){
     return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) :
-        (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )
-        }
-      </section>
+      <HashRouter>
+        <Route path="/about" component={About}/>
+        <Route path="/"  exact={true} component={Home} />
+      </HashRouter>
     );
   }
 }
